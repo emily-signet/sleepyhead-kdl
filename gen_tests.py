91545ea5f4b31f500becbb58ca0bffd92eb1171a
emily signet <emily@sibr.dev>
2022-04-30 13:33:35 -0300

    tests

---
+++ gen_tests.py
@@ -0,0 +1,10 @@
+import os
+import os.path
+
+for file in os.listdir("tests/input"):
+    (root, ext) = os.path.splitext(file)
+
+    if os.path.exists("tests/expected_kdl/" + file):
+        print(f"test_a_file!(include_str!(\"{'input/' + file}\"), {root});")
+    else:
+        print(f"test_a_file!(fail include_str!(\"{'input/' + file}\"), {root});")
